78c615549fd82a30ac9c3ac62a8978d1299c3697
vinux84 <vincentb0824@gmail.com>
2024-02-29 15:59:10 -0800

    added interrupt handler for button

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from machine import Pin
-from time import sleep 
+import machine
+import utime
 
 # declare pins here
+on_off_button = machine.Pin(10, machine.Pin.IN, machine.Pin.PULL_DOWN) # Pin 10 is just a placeholder. TBD on pin
+
+
+
+pressed_button = False
 
 def drink_two():
     pass
@@ -38,4 +43,15 @@ def server(action, limit_Switch):
     # as well as detect limit swtiches to stop the motor. This can be done through threading or anycio 
     # this will have input variables that will determine up or down
 
-def 
+def button_handler(pin):
+    global pressed_button
+    on_off_button.irq(handler=None)
+    utime.sleep_ms(250)
+    on_off_button.irq(trigger=machine.Pin.IRQ_RISING, handler=button_handler)
+    if not pressed_button:
+        # main logic
+        pressed_button = True
+    else:
+        pressed_button = False
+
+on_off_button.irq(trigger=machine.Pin.IRQ_RISING, handler=button_handler)
