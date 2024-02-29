521ff77ee26ed1a848d4af95132b4e543138da62
vinux84 <vincentb0824@gmail.com>
2024-02-29 15:25:20 -0800

    adding basic functions

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
-#start here
+from machine import Pin
+from time import sleep 
+
+# declare pins here
+
+def drink_two():
+    pass
+
+def drink_one():
+    pass
+
+def drink(drink_number):
+    pass
+
+def spout_down():
+    pass
+
+def spout_up():
+    pass 
+
+def pour(action, drink):
+    pass
+    # this will control the spout up or down
+    # after down can you input variable drink
+
+def limit_switch():
+    pass 
+
+def server_up():
+    pass
+
+def server_down():
+    pass
+
+def server(action, limit_Switch):
+    pass 
+    # this will control the cup server motor through action input
+    # as well as detect limit swtiches to stop the motor. This can be done through threading or anycio 
+    # this will have input variables that will determine up or down
+
+def 
