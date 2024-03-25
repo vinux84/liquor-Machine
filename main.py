b4d733fa8c7cbee0db23e22c65a0fba31edad05f
Vincent <vincentb0824@gmail.com>
2024-03-25 13:10:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,20 +5,22 @@ import _thread
 drink_one = machine.Pin(26, machine.Pin.IN, machine.Pin.PULL_DOWN)
 drink_two = machine.Pin(27, machine.Pin.IN, machine.Pin.PULL_DOWN)
 
-ir_sensor = machine.Pin(22, machine.Pin.IN, machine.Pin.PULL_DOWN) 
+ir_sensor = machine.Pin(22, machine.Pin.IN, machine.Pin.PULL_DOWN)
 
 limit_switch_top = machine.Pin(16, machine.Pin.IN, machine.Pin.PULL_UP) 
-limit_switch_bottom = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_UP) 
+limit_switch_bottom = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_UP)
 
 server_motor_one = machine.Pin(19, machine.Pin.OUT)
 server_motor_two = machine.Pin(18, machine.Pin.OUT)
 
 spout = machine.PWM(machine.Pin(12))
-  
-spout.freq(50) 
+spout.freq(50)
 
-drink_one_pump = machine.Pin(10, machine.Pin.OUT) # Pin 10 is just a placeholder. TBD on pin
-drink_two_pump = machine.Pin(10, machine.Pin.OUT) # Pin 10 is just a placeholder. TBD on pin
+drink_one_pump_a = machine.Pin(4, machine.Pin.OUT)
+drink_one_pump_b = machine.Pin(5, machine.Pin.OUT)
+
+drink_two_pump_a = machine.Pin(6, machine.Pin.OUT) 
+drink_two_pump_b = machine.Pin(7, machine.Pin.OUT) 
 
 drink_one_button = False
 drink_two_button = False 
@@ -32,13 +34,13 @@ bottom_limit_switch = False
     
 def dispense_drink(type_drink):
     if type_drink == 'one':
-        pump_on(drink_one_pump)
-        utime.sleep(3)              
-        pump_off(drink_one_pump)
+        pump_on(drink_one_pump_a, drink_one_pump_b)
+        utime.sleep(2.5)              
+        pump_off(drink_one_pump_a, drink_one_pump_b)
     elif type_drink == 'two':
-        pump_on(drink_two_pump)
+        pump_on(drink_two_pump_a, drink_two_pump_b)
         utime.sleep(3)              
-        pump_off(drink_two_pump)
+        pump_off(drink_two_pump_a, drink_two_pump_b)
        
 def spout_down(): 
     spout.duty_u16(6000)
@@ -50,13 +52,13 @@ def spout_up():
     utime.sleep(2)
     spout.deinit()
 
-def pump_off(pump_number):
-    pump_number.value(0)
-    pump_number.value(0)
-
-def pump_on(pump_number):
-    pump_number.value(0)
-    pump_number.value(1)
+def pump_on(pump_a, pump_b):
+    pump_a.value(1)
+    pump_b.value(0)
+    
+def pump_off(pump_a, pump_b):
+    pump_a.value(0)
+    pump_b.value(0)
 
 def server_stop():
     server_motor_one.value(0)
@@ -69,23 +71,6 @@ def server_up():
 def server_down():
     server_motor_one.value(1)
     server_motor_two.value(0)
-
-# begin testing function
-def movement(direction, time):
-    if direction == "d":
-        server_down()
-        utime.sleep(time)
-        server_stop()
-    elif direction == "u":
-        server_up()
-        utime.sleep(time)
-        server_stop()
-
-#server_stop()
-
-#movement('d', .25)
-#movement('u', .25)
-# end testing function
 
 def limit_switch_thread():
     global top_limit_switch
@@ -110,7 +95,7 @@ def main(type_drink):
                     utime.sleep(1)
                     spout_down()
                     utime.sleep(1)
-                    #dispense_drink(type_drink)
+                    dispense_drink(type_drink)
                     utime.sleep(4)              
                     spout_up()
                     utime.sleep(1)
@@ -119,18 +104,12 @@ def main(type_drink):
                         if top_limit_switch == True:
                             server_stop()
                             machine.reset()        
-                    
-
-    else:
-        print("Please place cup on holder")                        
-
+                
 def reset():
-    #pump_off(drink_one_pump)
-    #pump_off(drink_two_pump)
+    pump_off(drink_one_pump_a, drink_one_pump_b)
+    pump_off(drink_two_pump_a, drink_two_pump_b)
     server_stop()
-    utime.sleep(.50)
     spout_up()
-    utime.sleep(.50)
     if limit_switch_top.value() == 1:
         server_up()
         while True:
@@ -162,8 +141,9 @@ def button_handler_two(pin):
     else:
         drink_two_button = False
 
-reset() # for some reason when reset function ran, did not pass it to drink one push button / wait 
+reset() 
 
 drink_one.irq(trigger=machine.Pin.IRQ_RISING, handler=button_handler_one)
-#drink_two.irq(trigger=machine.Pin.IRQ_RISING, handler=button_handler_two)
+drink_two.irq(trigger=machine.Pin.IRQ_RISING, handler=button_handler_two)
+
 
